a7e59ade1313036c82e29db366576cdc93b3e146
name <varunbrahmane30@gmail.com>
2021-09-18 20:39:35 +0530

    [varun] added: notes interface for apis def.

--- RepositoryLayer/Interface/INotesRL.cs
+++ RepositoryLayer/Interface/INotesRL.cs
@@ -8,10 +8,14 @@ namespace RepositoryLayer.Interface
 {
     public interface INotesRL
     {
-        List<Notes> getAllNotes(long token);
+        List<Notes> getAllNotes(long id);
         public bool CreateNotes(NotesModel notesModel, long userId);
         public bool DeleteNotes(long id, long userId);
         public bool UpdateNotes(long id, long userId, NotesModel notesModel);
+        public bool IsPinned(long Id, long noteId, bool value);
+        public bool ChangeColor(long id, long noteId, string color);
+        public bool IsArchive(long id, long noteId, bool value);
+        public bool IsTrash(long id, long noteId, bool value);
 
     }
 }
