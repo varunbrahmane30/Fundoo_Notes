70ee6923f7b1492a72d6359c113661f18914a113
name <varunbrahmane30@gmail.com>
2021-09-18 20:24:47 +0530

    [varun] added: pinned, trash , archive apis.

--- BusinessLayer/Interface/INotesBL.cs
+++ BusinessLayer/Interface/INotesBL.cs
@@ -6,9 +6,14 @@ namespace BusinessLayer.Interface
 {
     public interface INotesBL 
     {
-        List<Notes> getAllNotes(long token);
+        List<Notes> getAllNotes(long id);
         public bool CreateNotes(NotesModel notesModel, long userId);
         public bool DeleteNotes(long id, long userId);
         public bool UpdateNotes(long id,long userId, NotesModel notesModel);
+        public bool IsPinned(long Id, long noteId,bool value);
+        public bool ChangeColor(long Id, long noteId,string color);
+        public bool IsArchive(long Id, long noteId, bool value);
+        public bool IsTrash(long Id, long noteId, bool value);
+
     }
 }
