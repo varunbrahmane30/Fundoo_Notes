429817c53e75dd4e3f98be0377d57e28d9860b35
name <varunbrahmane30@gmail.com>
2021-09-18 20:38:08 +0530

    [varun] added: user context for db operation and table info

--- RepositoryLayer/Context/UserContext.cs
+++ RepositoryLayer/Context/UserContext.cs
@@ -14,6 +14,7 @@ namespace RepositoryLayer.Context
 
         public DbSet<User> Users { get; set; }
         public DbSet<Notes> Notes { get; set; }
+        public DbSet<Collaboration> collaborations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
