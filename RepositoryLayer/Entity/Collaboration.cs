06826c6ed95967adfb2e407ebf7603737ce1d794
name <varunbrahmane30@gmail.com>
2021-09-18 20:46:01 +0530

    [varun] added: collaboration model.

---
+++ RepositoryLayer/Entity/Collaboration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class Collaboration
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id  { get; set; }
+        public long CollaborationID { get; set; }
+        public long Userid { get; set; }
+        public User User { get; set; }
+        public long Noteid { get; set; }
+        public Notes Note { get; set; }
+    }
+}
