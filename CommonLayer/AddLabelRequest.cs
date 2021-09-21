f9f7b1e2aad51fc5c1786e6e12d2ddfbe01d7a49
name <varunbrahmane30@gmail.com>
2021-09-21 10:23:37 +0530

    [varun] added: label model for get label .

---
+++ CommonLayer/AddLabelRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class AddLabelRequest
+    {
+        public String Label { get; set; }
+    }
+}
