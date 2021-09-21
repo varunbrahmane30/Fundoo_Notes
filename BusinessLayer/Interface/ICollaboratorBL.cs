4c0b872665d71b8603b86fffa7b4c8e340aaaf93
name <varunbrahmane30@gmail.com>
2021-09-21 10:18:47 +0530

    [varun] added: Collborators interface Crud apis.

---
+++ BusinessLayer/Interface/ICollaboratorBL.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Interface
+{
+    public interface ICollaboratorBL
+    {
+        public List<string> GetCollaboratorsId(long noteId);
+        public bool AddCollborators(long userId, long id, AddCollaboratorRequest collaborators);
+        public bool DeleteCollborators(long userId, long id, AddCollaboratorRequest collaborators);
+    }
+
+}
