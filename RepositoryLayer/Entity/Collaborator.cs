79e6cee7cedf8d7fe944a3fcfa9c168a724715f2
name <varunbrahmane30@gmail.com>
2021-09-21 10:29:09 +0530

    [varun] added: collborator email and ids.

---
+++ RepositoryLayer/Entity/Collaborator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepositoryLayer.Entity
+{
+    public class Collaborator
+    {
+        [Key, Column(Order = 0)]
+        public long UserId { get; set; }
+        public User User { get; set; }
+
+        [Key, Column(Order = 1)]
+        public long NoteId { get; set; }
+        public Notes Note { get; set; }
+
+    }
+}
