f7705950bd52d33f8ad4c67d719dec5c03d6c150
name <varunbrahmane30@gmail.com>
2021-09-21 10:21:00 +0530

    [varun] added: label methods to validation and redirect to RL.

---
+++ BusinessLayer/Services/LabelBL.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class LabelBL : ILabelBL
+    {
+        
+        private ILabelRL _labelRL;
+        public LabelBL(ILabelRL labelRL)
+        {
+            this._labelRL = labelRL;
+           
+        }
+        public bool AddLabel(long noteId,AddLabelRequest label)
+        {
+            try
+            {
+                return _labelRL.AddLabel(noteId,label);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+           
+        }
+
+        public bool DeleteLabel(long noteId, AddLabelRequest label)
+        {
+            try
+            {
+                return _labelRL.DeleteLabel(noteId, label);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public bool UpdateLabel(long noteId, AddLabelRequest label)
+        {
+            try
+            {
+                return _labelRL.UpdateLabel(noteId, label);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
