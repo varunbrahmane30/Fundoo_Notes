545e9f68764a52919104e9f5e106bb5e63dcc732
name <varunbrahmane30@gmail.com>
2021-09-21 10:31:10 +0530

    [varun] added: noteslabel entity for reference to label

---
+++ RepositoryLayer/Entity/NotesLabel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class NotesLabel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+        public long NotesId { get; set; }
+        public Notes Notes { get; set; }
+        public long LabelId { get; set; }
+        public Label Label { get; set; }
+    }
+}
