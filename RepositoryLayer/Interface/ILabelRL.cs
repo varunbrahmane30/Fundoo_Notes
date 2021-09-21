ff057764230a75a069e6ecdd02f45a57d453721b
name <varunbrahmane30@gmail.com>
2021-09-21 10:32:12 +0530

    [varun] added: label operation in RL

---
+++ RepositoryLayer/Interface/ILabelRL.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ILabelRL
+    {
+        public bool AddLabel(long noteId,AddLabelRequest label);
+        public bool DeleteLabel(long noteId, AddLabelRequest label);
+        public bool UpdateLabel(long noteId, AddLabelRequest label);
+
+    }
+}
