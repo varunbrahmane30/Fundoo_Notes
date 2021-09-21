ee62a68edbd82842ee03a5f8b004139de7dfd57c
name <varunbrahmane30@gmail.com>
2021-09-21 10:31:51 +0530

    [varun] added: colloborator db operation

---
+++ RepositoryLayer/Interface/ICollaboratorRL.cs
@@ -0,0 +1,14 @@
+﻿using RepositoryLayer.Entity;
+using System.Collections.Generic;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ICollaboratorRL
+    {
+        
+        public bool AddCollaborator(long noteId, long collaboratorId);
+        public List<string> GetCollaboratorsId(long noteId);
+        public bool DeleteCollaborator(long noteId, long collaboratorId);
+
+    }
+}
