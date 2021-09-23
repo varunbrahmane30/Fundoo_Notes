81ec1d1fecd530ec0720fdfb8875c7b28db1e400
name <varunbrahmane30@gmail.com>
2021-09-23 14:37:44 +0530

    [varun] added: msmq and label models.

---
+++ CommonLayer/CollaborationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer
+{
+    class CollaborationModel
+    {
+        [Required]
+        public String Email { get; set; }
+    }
+}
